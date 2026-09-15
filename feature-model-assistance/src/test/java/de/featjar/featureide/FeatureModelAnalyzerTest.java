9217464ed8d67d50de5ef63182422ae2c632b752
Zenkri, Ines <ines.zenkri@vwfs.com>
2026-09-15 13:25:02 +0200

    fix: ignore only the root level test folder

---
+++ feature-model-assistance/src/test/java/de/featjar/featureide/FeatureModelAnalyzerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2026 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-feature-model-assistance.
+ *
+ * feature-model-assistance is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * feature-model-assistance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with feature-model-assistance. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-feature-model-assistance> for further information.
+ */
+package de.featjar.featureide;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import de.featjar.Common;
+import de.featjar.base.data.Void;
+import de.featjar.base.io.text.StringTextFormat;
+import de.featjar.base.tree.DataTree;
+import de.featjar.feature.model.IConstraint;
+import de.featjar.feature.model.IFeature;
+import de.featjar.feature.model.io.uvl.UVLFeatureModelFormat;
+import de.featjar.formula.structure.connective.And;
+import de.featjar.formula.structure.connective.Implies;
+import de.featjar.formula.structure.connective.Not;
+import java.math.BigInteger;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class FeatureModelAnalyzerTest {
+
+    @SuppressWarnings("unused")
+    @Test
+    public void testFeatureModelAnalyzer() {
+        
+    }
+}
