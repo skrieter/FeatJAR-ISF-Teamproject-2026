2037eb832c6bde5f15cb8cec18e86fdeae04473e
Sebastian Krieter <sebastian.krieter@tu-braunschweig.de>
2026-09-17 10:56:50 +0200

    fix: adds licence header

--- gui-client-web/src/main/java/de/featjar/gui/bin/GuiWebBinary.java
+++ gui-client-web/src/main/java/de/featjar/gui/bin/GuiWebBinary.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2026 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-gui-client-web.
+ *
+ * gui-client-web is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * gui-client-web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with gui-client-web. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE> for further information.
+ */
 package de.featjar.gui.bin;
 
 import de.featjar.base.env.ABinary;
