1d73ff61d3a8be15a03a55fc90f184df38b1f637
Sebastian Krieter <sebastian.krieter@tu-braunschweig.de>
2026-09-17 10:58:11 +0200

    fix: fixes port arguments for gui server command

--- gui/src/main/java/de/featjar/gui/GuiServer.java
+++ gui/src/main/java/de/featjar/gui/GuiServer.java
@@ -67,7 +67,8 @@ public class GuiServer {
                 "-cp",
                 System.getProperty("user.home") + "/.featjar-bin/gui/server/*",
                 "de.featjar.gui.FeatureModelWebsocketLauncher",
-                "-p " + String.valueOf(port));
+                "--port",
+                String.valueOf(port));
 
         FeatJAR.log().info("Server is starting...");
         ProcessBuilder pb = new ProcessBuilder(command);
