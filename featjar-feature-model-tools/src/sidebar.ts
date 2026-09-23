f1a054971d858c74a4e53b3a786194565bc5415a
Hassan Janati <h.janati@tu-braunschweig.de>
2026-09-23 15:32:58 +0200

    feat: improve UVL sidebar refresh and icon (#54)

--- featjar-feature-model-tools/src/sidebar.ts
+++ featjar-feature-model-tools/src/sidebar.ts
@@ -1,6 +1,10 @@
 import * as vscode from 'vscode';
 
 class UvlFileProvider implements vscode.TreeDataProvider<vscode.Uri> {
+    //die private Variable __onDidChangeTreeData ist von AI vorgeschlagen. Damit ich die regelmäßige Refresh benutzen kann.
+    private _onDidChangeTreeData =new vscode.EventEmitter<vscode.Uri | undefined>();
+
+    readonly onDidChangeTreeData =this._onDidChangeTreeData.event;
 
     getTreeItem(element: vscode.Uri): vscode.TreeItem {
         return new vscode.TreeItem(element);
@@ -10,16 +14,22 @@ class UvlFileProvider implements vscode.TreeDataProvider<vscode.Uri> {
         const files = await vscode.workspace.findFiles('**/*.uvl');
         return files;
     }
+    refresh(): void {
+        this._onDidChangeTreeData.fire(undefined);
+    }
 }
 
 export function registerSidebar(context: vscode.ExtensionContext) {
 
     const uvlFileProvider = new UvlFileProvider();
-
+    const refreshCommand = vscode.commands.registerCommand('featjar-extension.refreshUvlFiles',() => {
+        uvlFileProvider.refresh();
+    }
+    );
     const uvlTree = vscode.window.registerTreeDataProvider(
         'uvlFiles',
         uvlFileProvider
     );
 
-    context.subscriptions.push(uvlTree);
+    context.subscriptions.push(uvlTree, refreshCommand);
 }
